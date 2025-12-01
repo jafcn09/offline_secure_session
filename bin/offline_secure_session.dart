0c90d92196bf3578f233ee83b31ca42ccfc1aec4
jafcn09 <jafcnepamace24@gmail.com>
2025-12-01 12:33:03 -0500

    ibrary

--- bin/offline_secure_session.dart
+++
@@ -1,5 +0,0 @@
-import 'package:offline_secure_session/offline_secure_session.dart' as offline_secure_session;
-
-void main(List<String> arguments) {
-  print('Hello world: ${offline_secure_session.calculate()}!');
-}
